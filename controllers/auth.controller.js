0056beecb4f62cb4811cc62a7eff4a78b48a0a97
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2025-06-22 19:09:21 -0400

    auth.controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,9 @@
+const loginUser = (req, res) => {
+    res.send('Auth controller');
+}
+
+
+
+module.exports = {
+    loginUser,
+}
--- router/auth.js
+++ router/auth.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
+const { loginUser } = require("../controllers/auth.controller");
 
 const authRouter = Router();
 
-authRouter.get('/', (req, res) => {
-    res.send('auth server');
-});
+// authRouter.get('/login', (req, res) => { 
+//     res.send('auth server');
+// });
 
+// authRouter.post('/login', (req, res) => { // se oculta información en la url
+
+//     // verificar los datos y formato específico
+
+//     // buscar el registro en la base de datos
+
+//     // verificar la contraseña
+
+
+//     res.send('auth server');
+// });
+
+
+authRouter.get('/login', loginUser);
 
 
 module.exports = authRouter;
