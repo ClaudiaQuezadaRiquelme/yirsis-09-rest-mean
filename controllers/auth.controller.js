5ee8b64fd8e944bcd836f053f49108b9cee42e49
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2025-07-15 22:39:47 -0400

    add user without password protection

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,40 @@
+const userModel = require('../models/usuario')
+
 const loginUser = (req, res) => {
     res.send('Login controller');
 }
 
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
     console.log("registerUser: ", req.body);
     
     const {email, password, username} = req.body; // si recibo más datos dentro del request, con la desestructuración evito guardar datos que no he planificado manejar, así evito problemas de seguridad.
+
+    try {
+        let user = await userModel.findOne({ email });
+        if (user) {
+            return res.status(501).json({
+                ok: false,
+                msg: 'Correo ya registrado'
+            });
+        } else {
+            const newUser = new userModel({
+                email, password, username
+            });
+            await newUser.save();
+            res.json({
+                ok: true,
+                email, password, username,
+                msg: 'Usuario creado.'
+            });
+        }
+    } catch (error) {
+        console.log('Error en el registro de usuario.');
+        res.json({
+            ok: false,
+            msg: 'Error al registrar usuario.'
+        });
+    }
     console.log({email, password, username} );
-    
     res.json({ok: true, email, username, password});
 }
 
